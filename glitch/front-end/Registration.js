3567741ef8cc6e4c7a1d76e7ccc93a811ef0c9ee
Anas-1077402 <145554176+Anas-1077402@users.noreply.github.com>
2024-05-01 12:53:11 +0200

    Added Registration, had problems with branch now fixxed.

--- glitch/front-end/Registration.js
+++ glitch/front-end/Registration.js
@@ -1,1 +1,53 @@
-test
+import React, {useState} from 'react';
+
+const RegistrationForm = () => {
+    const[username, setUsername] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+       // console.log("Username", username);
+        //console.log("Email", email);
+        //console.log("Password", password);
+
+        setUsername('');
+        setEmail('');
+        setPassword('');
+    };
+    return (
+        <div>
+          <h2>Registratieformulier</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Gebruikersnaam:</label>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Wachtwoord:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Registreren</button>
+          </form>
+        </div>
+      );
+};
+
+export default RegistrationForm;
