a1094c61387130e142270a9268f3f9d964905ff5
Anas-1077402 <145554176+Anas-1077402@users.noreply.github.com>
2024-05-07 13:44:25 +0200

    Inserting GameUser from registrationform now working.

--- glitch/authentication/models.py
+++ glitch/authentication/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.utils import timezone
 
+class GameUser(models.Model):
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_teacher = models.BooleanField(default=0)
+    
+    def __str__(self):
+        return self.username
--- glitch/authentication/views.py
+++ glitch/authentication/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
 from django.db import connection
-from .models import User
+from .models import GameUser
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -14,18 +14,19 @@ def register_user(request):
             is_superuser = 0
             is_staff = 0
             is_active = 1
-            first_name = request.POST.get('first_name', '')
-            last_name = request.POST.get('last_name', '')
+            first_name = request.data.get('first_name', '')
+            last_name = request.data.get('last_name', '')
             username = request.data.get('username', '')
             email = request.data.get('email', '')
             password = request.data.get('password', '')
+            is_teacher = GameUser().is_teacher
 
-            sql = """ INSERT INTO game_user (is_active,is_staff,is_superuser,first_name, last_name, email, username, password, date_joined)
-            VALUES (%s,%s,%s,%s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"""
+            sql = """ INSERT INTO game_user (is_active, is_staff, is_superuser, first_name, last_name, email, username, password, date_joined, is_teacher)
+            VALUES (%s,%s,%s,%s, %s, %s, %s, %s, CURRENT_TIMESTAMP, %s)"""
 
         
         with connection.cursor() as cursor:
-                cursor.execute(sql, [is_active,is_staff,is_superuser,first_name, last_name, email, username, password])
+                cursor.execute(sql, [is_active, is_staff, is_superuser, first_name, last_name, email, username, password, is_teacher])
 
         return JsonResponse({'message': 'Succesvol geregistreerd'}, status=200)
  
