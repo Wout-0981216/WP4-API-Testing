1f98c0693cc3e8aca548a4729e0293e8bb04c128
Anas-1077402 <145554176+Anas-1077402@users.noreply.github.com>
2024-05-20 17:04:55 +0200

    Added activities-module file and page route

--- glitch/front-end/App.js
+++ glitch/front-end/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RegistrationForm from './Registration';
-import Assignment from './assignment';
+import Activities from './activities-module';
 
 
 export default function App() {
@@ -12,6 +12,7 @@ export default function App() {
       <Routes>
       <Route path="/register" element={<RegistrationForm />} />
       <Route path="/concept-opdracht" element={<Assignment />} />
+      <Route path="/activities-module" element={<Activities />} />
       </Routes>
   </BrowserRouter>
   );
---
+++ glitch/front-end/activities-module.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ActiviteitenPage = () => {
+
+    return (
+        <div>
+            <h1>Activiteiten</h1>
+        </div>
+    )
+};
