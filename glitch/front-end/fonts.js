3a9c985e6c15f9a611a369832814211736ad6d47
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-05-22 14:24:31 +0200

    deleted random spawned already deleted fies

--- glitch/front-end/fonts.js
+++
@@ -1,8 +0,0 @@
-import { Font } from 'expo-font';
-
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    'jomhuria': require('./src/fonts/Jomhuria.ttf'),
-    'Arial': require('./src/fonts/Arial.ttf'),
-  });
-};
